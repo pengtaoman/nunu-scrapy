11afe478afe48b1dea3ac52d02b4d3de51cfae6a
pengtaoman <pengtaoman@163.com>
2023-01-11 15:41:04 +0800

    asdfasdfasd

--- util/pandasdatautil.py
+++ util/pandasdatautil.py
@@ -7,6 +7,7 @@ df = pd.read_json("../json/movie.json")
 df.dtypes
 # df.set_index('', inplace=True)
 print(type(df))
+print(df.size)
 print(df.info(verbose=False, memory_usage='deep'))
 cols = df.columns
 inx = df.index
@@ -16,14 +17,17 @@ inx = df.index
 
 # df.loc[lambda df : (df["name"]<=30) & (df["yWendu"]>=15), :]
 filterdf = df[df.name.str.contains('霸王')]
+print(filterdf)
 
-data = [[50, True], [40, False], [30, False]]
-label_rows = ["Sally", "Mary", "John"]
-label_cols = ["age", "qualified"]
+print(df.size)
 
-dataf = pd.DataFrame(data, label_rows, label_cols)
-print('#'*20)
-print(dataf.loc["Mary", "age"])
+# data = [[50, True], [40, False], [30, False]]
+# label_rows = ["Sally", "Mary", "John"]
+# label_cols = ["age", "qualified"]
+
+# dataf = pd.DataFrame(data, label_rows, label_cols)
+# print('#'*20)
+# print(dataf.loc["Mary", "age"])
 
 ts = pd.Series(np.random.randn(1000), index=pd.date_range('1/1/2000', periods=1000))
 ts = ts.cumsum()
